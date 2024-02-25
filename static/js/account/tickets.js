d587e7c491085e8bd77807ab964d71ba2470587d
Robert Thompson <robertdavidthompson632@gmail.com>
2024-02-24 22:09:09 -0500

    Added feature to display the venue via a hyper link in a modal

--- static/js/account/tickets.js
+++ static/js/account/tickets.js
@@ -1,88 +1,131 @@
 class TicketsView {
   constructor(user_ticket_data) {
-    this.update(user_ticket_data);
+    this.user_ticket_data = user_ticket_data;
+    this.initialize();
   }
 
-  update(tickets) {
-    this.updateTable(tickets);
-  }
-
-  updateTable(tickets) {
-    console.log("Tickets:", JSON.stringify(tickets, null, 2));
-
-    const $ticketsTable = $("#ticketsTable").empty();
-    const tableBody = $('<tbody id="ticketsTableBody"></tbody>').appendTo($ticketsTable);
-
-    const eventsGroup = this.groupTicketsByEvent(tickets);
-
-    Object.values(eventsGroup).forEach((group) => {
-      tableBody.append(this.createEventRow(group));
-      group.forEach((ticket) => tableBody.append(this.createDetailsRow(ticket)));
-    });
-
+  initialize() {
+    this.renderTable();
+    this.initializeVenueModal();
     this.initializeToggleDetails();
   }
 
-  groupTicketsByEvent(tickets) {
-    return tickets.reduce((acc, ticket) => {
-      (acc[ticket.event_id] = acc[ticket.event_id] || []).push(ticket);
-      return acc;
-    }, {});
+  renderTable() {
+    console.log("Tickets:", JSON.stringify(this.user_ticket_data, null, 2));
+    const $ticketsTable = $("#ticketsTable").empty();
+    const tableBody = $('<tbody id="ticketsTableBody"></tbody>').appendTo(
+      $ticketsTable
+    );
+    const eventsGroup = this.groupTicketsByEvent();
+
+    eventsGroup.forEach((group) => {
+      tableBody.append(this.createEventRow(group));
+      group.forEach((ticket) =>
+        tableBody.append(this.createDetailsRow(ticket))
+      );
+    });
+  }
+
+  groupTicketsByEvent() {
+    return Object.values(
+      this.user_ticket_data.reduce((acc, ticket) => {
+        (acc[ticket.event_id] = acc[ticket.event_id] || []).push(ticket);
+        return acc;
+      }, {})
+    );
   }
 
   createEventRow(group) {
     const firstTicket = group[0];
-    const totalPrice = group.reduce((acc, ticket) => acc + ticket.seat_price * ticket.quantity, 0).toFixed(2);
-    const venueAddress = `${firstTicket.venue_street}, ${firstTicket.venue_city}, ${firstTicket.venue_state}, ${firstTicket.venue_zip_code}`;
-    const startDate = convertDateToHumanReadable(firstTicket.start_date);
-    const startTime = convertTo12HourFormat(firstTicket.start_time);
+    const venueInfo = this.constructVenueInfo(firstTicket);
 
     return $(`
       <tr class="event-row" data-event-id="${firstTicket.event_id}">
         <td>
           <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center">
-              <img src="/static/img/events/${firstTicket.event_image}" alt="Event Image" style="width: 100px; height: auto; margin-right: 20px;">
+              <img src="/static/img/events/${
+                firstTicket.event_image
+              }" alt="Event Image" style="width: 100px; height: auto; margin-right: 20px;">
               <div>
                 <h5>${firstTicket.event_name} - ${firstTicket.artist}</h5>
-                <p>${firstTicket.venue_name} - ${venueAddress}<br>${startDate} at ${startTime}</p>
-                <button type="button" class="btn btn-link toggle-details" data-event-name="${firstTicket.event_name}">
+                <p>${venueInfo}<br>${this.formatDate(
+      firstTicket.start_date
+    )} at ${this.formatTime(firstTicket.start_time)}</p>
+                <button type="button" class="btn btn-link toggle-details" data-event-name="${
+                  firstTicket.event_name
+                }">
                   <i class="fas fa-caret-down"></i> View Details
                 </button>
               </div>
             </div>
-            <div class="total-price">
-              <strong>Total Price: $${totalPrice}</strong>
-            </div>
+            <div class="total-price"><strong>Total Price: $${this.calculateTotalPrice(
+              group
+            )}</strong></div>
           </div>
         </td>
       </tr>
     `);
   }
 
+  constructVenueInfo(firstTicket) {
+    return `<a href="#" class="venue-link" data-toggle="modal" data-target="#venueModal" 
+            data-venue-name="${firstTicket.venue_name}" 
+            data-venue-image="/static/img/venues/${firstTicket.venue_image}">${firstTicket.venue_name} - 
+            ${firstTicket.venue_street}, ${firstTicket.venue_city}, ${firstTicket.venue_state}, ${firstTicket.venue_zip_code}</a>`;
+  }
+
+  calculateTotalPrice(group) {
+    return group
+      .reduce((acc, ticket) => acc + ticket.seat_price * ticket.quantity, 0)
+      .toFixed(2);
+  }
+
   createDetailsRow(ticket) {
     return $(`
-      <tr class="details-row" data-event-name="${ticket.event_name}" style="display: none;">
-        <td>
-          <div class="details-content">
-            <p><strong>Ticket ID:</strong> ${ticket.ticket_id}</p>
-            <p><strong>Order Date:</strong> ${ticket.ticket_order_date}</p>
-            <p><strong>Seat Number:</strong> ${ticket.seat_number}</p>
-            <p><strong>Seat Price:</strong> $${ticket.seat_price.toFixed(2)}</p>
-            <p><strong>Quantity:</strong> ${ticket.quantity}</p>
-          </div>
-        </td>
+      <tr class="details-row" data-event-name="${
+        ticket.event_name
+      }" style="display: none;">
+        <td>${this.ticketDetails(ticket)}</td>
       </tr>
     `);
+  }
+
+  ticketDetails(ticket) {
+    return `
+      <div class="details-content">
+        <p><strong>Ticket ID:</strong> ${ticket.ticket_id}</p>
+        <p><strong>Order Date:</strong> ${ticket.ticket_order_date}</p>
+        <p><strong>Seat Number:</strong> ${ticket.seat_number}</p>
+        <p><strong>Seat Price:</strong> $${ticket.seat_price.toFixed(2)}</p>
+        <p><strong>Quantity:</strong> ${ticket.quantity}</p>
+      </div>
+    `;
   }
 
   initializeToggleDetails() {
     $(".toggle-details").on("click", function () {
       const eventName = $(this).data("event-name");
-      const detailsRows = $(`.details-row[data-event-name="${eventName}"]`);
-      detailsRows.toggle();
-      $(this).find("i").toggleClass("fa-caret-down", detailsRows.is(":visible")).toggleClass("fa-caret-up", !detailsRows.is(":visible"));
+      $(`.details-row[data-event-name="${eventName}"]`).toggle();
+      $(this).find("i").toggleClass("fa-caret-down fa-caret-up");
     });
+  }
+
+  initializeVenueModal() {
+    $("#venueModal").on("show.bs.modal", function (event) {
+      const button = $(event.relatedTarget);
+      const modal = $(this);
+      modal.find(".modal-title").text(button.data("venue-name"));
+      modal.find("#venueImage").attr("src", button.data("venue-image"));
+    });
+  }
+
+  formatTime(time) {
+    return convertTo12HourFormat(time);
+  }
+
+  formatDate(dateStr) {
+    return convertDateToHumanReadable(dateStr);
   }
 }
 
